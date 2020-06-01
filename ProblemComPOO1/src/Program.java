2b3ed21e5d99eacfe7f97a067d0d2e38d2f97da5
Valmir Santana <valmirsantanape@gmail.com>
2020-05-31 22:56:47 -0300

    POO
    resolvendo prblemas com triangulos 2

--- ProblemComPOO1/src/Program.java
+++ ProblemComPOO1/src/Program.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author valmi
+ * @author DevValmir
  */
 public class Program {
 
@@ -19,23 +19,26 @@ public class Program {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
-        Triangulo x = new Triangulo();
+        Triangulo x, y;
+        x = new Triangulo();
+        y = new Triangulo();
         System.out.println("Digite o lado do driangulo X: ");
         x.a = sc.nextDouble();
         x.b = sc.nextDouble();
         x.c = sc.nextDouble();
         
-        Triangulo y = new Triangulo();
+        
         System.out.println("Digite o lado do driangulo Y: ");
         y.a = sc.nextDouble();
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
         
-        double p = (x.a+x.b+x.c)/2;
-        double areaX = Math.sqrt(p*(p-x.a)*(p-x.b)*(p-x.c));
         
-        p = (y.a+y.b+y.c)/2;
-        double areaY = Math.sqrt(p*(p-y.a)*(p-y.a)*(p-y.c));
+        double areaX = x.area();
+        double areaY = y.area();
+        
+        System.out.println("Triangulo X: " + areaX);
+        System.out.println("Triangulo Y: " + areaY);
         
         if(areaX > areaY){
             System.out.println("Maior área é a do triangulo X: "+ areaX);
