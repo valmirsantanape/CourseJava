51ec94e8a3682df0c3b9e665406afa34de1491b3
Valmir Santana <valmirsantanape@gmail.com>
2020-06-04 23:16:41 -0300

    DesalocacaoMemoria
    vetores

---
+++ DesalocacaoMemoria/src/desalocacaomemoria/DesalocacaoMemoria.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desalocacaomemoria;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author valmi
+ */
+public class DesalocacaoMemoria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ola");
+        int num = sc.nextInt();
+    }
+    
+}
