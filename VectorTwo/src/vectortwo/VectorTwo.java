999e5790b893b2ea8e2d5ead0c0e8c6bf7ae78c7
Valmir Santana <valmirsantanape@gmail.com>
2020-06-10 19:24:43 -0300

    VetorDoTipoClasse

--- VectorTwo/src/vectortwo/VectorTwo.java
+++ VectorTwo/src/vectortwo/VectorTwo.java
@@ -5,6 +5,8 @@
  */
 package vectortwo;
 
+import java.util.Scanner;
+
 /**
  *
  * @author valmi
@@ -15,7 +17,30 @@ public class VectorTwo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entre com a quantidade de produtos que serão listados: ");
+        int n = sc.nextInt();
+        //"n" indica quantos produtos serão lidos 
+        Product[] vect = new Product[n]; // vetor do tipo classe
+        
+        for(int i = 0; i<n; i++){
+            sc.nextLine();
+            System.out.println("Nome do produto: ");
+            String name = sc.nextLine();
+            System.out.println("Preço do produto: ");
+            double price = sc.nextDouble();
+            
+            vect[i] = new Product(name, price);
+        }
+        
+        double sum = 0; 
+        for(int i=0; i<n; i++){
+            sum += vect[i].getPrice();
+         }
+        double avg = sum/n;
+        System.out.println("Media dos preços: " +avg);
+        sc.close();
     }
     
 }
