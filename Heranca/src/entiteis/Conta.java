c2ccc0408003d4b36aa3ad05b1775673958c9b30
Valmir Santana <valmirsantanape@gmail.com>
2020-06-23 00:46:32 -0300

    Protected
    Modificador de acesso

--- Heranca/src/entiteis/Conta.java
+++ Heranca/src/entiteis/Conta.java
@@ -10,5 +10,47 @@ package entiteis;
  * @author valmi
  */
 public class Conta {
+    private int conta;
+    private String nome;
+    protected double saldo;
     
+    public Conta(){
+        
+    }
+
+    public Conta(int conta, String nome, double saldo) {
+        this.conta = conta;
+        this.nome = nome;
+        this.saldo = saldo;
+    }
+
+    public int getConta() {
+        return conta;
+    }
+
+    public void setConta(int conta) {
+        this.conta = conta;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+    
+    
+    
+    public void saque(double conta){
+        saldo = saldo - conta;
+        
+    }
+    public void deposito(double conta){
+        saldo = saldo + conta;
+    }
 }
--- Heranca/src/entiteis/ContaEmpresa.java
+++ Heranca/src/entiteis/ContaEmpresa.java
@@ -9,6 +9,39 @@ package entiteis;
  *
  * @author valmi
  */
-public class ContaEmpresa {
+public class ContaEmpresa extends Conta{
+    //Função extends dá a classe ContaEmpresa
+    //todos os dados e comportamento 
+    //da classe Conta
+    private double emprestimo;
     
+    public ContaEmpresa(){
+        super();   
+    } 
+
+    public ContaEmpresa(double emprestimo) {
+        this.emprestimo = emprestimo;
+    }
+
+    public ContaEmpresa(int conta, String nome, double saldo, double emprestimo) {
+        super(conta, nome, saldo);
+        this.emprestimo = emprestimo;
+    }
+
+    
+
+    public double getEmprestimo() {
+        return emprestimo;
+    }
+
+    public void setEmprestimo(double emprestimo) {
+        this.emprestimo = emprestimo;
+    }
+    
+    public void emprestimo(double conta){
+        if(conta <= emprestimo){
+        saldo = saldo + emprestimo - 10;
+            deposito(conta);
+        }
+    }
 }
