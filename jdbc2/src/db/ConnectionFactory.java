f853dc3ed1291d23375f1e01450a069bb18f6f3c
Valmir Santana <valmirsantanape@gmail.com>
2020-07-30 22:56:06 -0300

    Update ConnectionFactory.java

--- jdbc2/src/db/ConnectionFactory.java
+++ jdbc2/src/db/ConnectionFactory.java
@@ -1,9 +1,11 @@
+package db;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package db;
+
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -20,7 +22,7 @@ import javax.swing.JOptionPane;
 public class ConnectionFactory {
     public Connection getConnection(){
         try {
-            return (Connection) DriverManager.getConnection("jdbc:mysql://localhost/coursejdbc?useTimezone=true&serverTimezone=UTC","root","12345");
+            return (Connection) DriverManager.getConnection("jdbc:mysql://localhost/coursejdbc?useTimezone=true&serverTimezone=UTC","root","");
         } catch (SQLException e) {
             //throw new RuntimeException(e);
             JOptionPane.showMessageDialog(null, "Falha na conexao " + e);
@@ -28,6 +30,8 @@ public class ConnectionFactory {
         return null;
         
     }
+
+    
 
    
 
